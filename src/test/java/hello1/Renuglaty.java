003255f61ab70eb3fe25bbfdb3f7c69f2a244c61
sandeepbutham <buthamsandeep123@gmail.com>
2023-06-11 19:31:28 +0530

    hhh

--- src/test/java/hello1/Renuglaty.java
+++ src/test/java/hello1/Renuglaty.java
@@ -4,14 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class Myone
+public class Renuglaty
 {
 
 
-
-
-    public static void main(String[] args) throws InterruptedException {
+@Test
+void testing() throws InterruptedException {
 
         System.setProperty("WebDriver.Chrome.driver", "Chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -37,9 +37,39 @@ public class Myone
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[text()='New Delhi, India']")).click();
 
-        driver.findElement(By.xpath("//*[text()='Search']")).click();
+    driver.findElement(By.xpath("//*[text()='Departure']")).click();
+    //*[@class='DayPicker-Caption']
 
-        System.out.println("hello");
+    Thread.sleep(2000);
+    String text = driver.findElement(By.xpath("//div[@class='DayPicker-Caption']")).getText();
+    Thread.sleep(2000);
+    while (!(text.equals("October 2023"))) {
+        driver.findElement(By.xpath("//*[@aria-label='Next Month']")).click();
+        text = driver.findElement(By.xpath("//div[@class='DayPicker-Caption']")).getText();
+
+    }
+    String date = "20";
+
+    driver.findElement(By.xpath("//p[text()='"+date+"']")).click();
+
+
+
+    driver.findElement(By.xpath("//*[text()='Return']")).click();
+
+    Thread.sleep(2000);
+    String text1 = driver.findElement(By.xpath("//div[@class='DayPicker-Caption']")).getText();
+    Thread.sleep(2000);
+    while (!(text1.equals("October 2023")))
+
+    {
+        driver.findElement(By.xpath("//span[@aria-label='Next Month']")).click();
+        text1 = driver.findElement(By.xpath("//div[@class='DayPicker-Caption']")).getText();
+    }
+
+    driver.findElement(By.xpath("//p[text()='21']")).click();
+
+
+    driver.findElement(By.xpath("//*[text()='Search']")).click();
 
     }
 
